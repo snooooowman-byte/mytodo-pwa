f5c82684d94784c7f22aa8868172e0f455491065
snooooowman-byte <snooooowman@gmail.com>
2026-05-29 00:02:20 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mytodo-v1';
+const CACHE = 'mytodo-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-180.png'];
 
 self.addEventListener('install', e => {
